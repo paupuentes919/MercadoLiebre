87c207c4f32e9cb6da7b5cd28c60955c3da2ba3e
paupuentes919 <paupuentes919@gmail.com>
2023-05-26 18:40:31 -0300

    adding navbar

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req,res) =>{
-    res.sendFile((__dirname + '/views/index.html')); 
+    res.sendFile((__dirname + '/views/home.html')); 
 });
 
 app.get('*', (req,res) =>{
